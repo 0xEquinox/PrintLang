5c00567d5317c596638f02a5ea1af70ccb138d44
0xEquinox <69062137+0xEquinox@users.noreply.github.com>
2021-04-30 08:18:12 -0700

    Added the code

---
+++ FirstProject/src/main.rs
@@ -0,0 +1,41 @@
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+
+fn main() {
+
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+
+    let mut count:u16 = 0;
+
+    println!("Input your guess: ");
+
+    loop {
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = guess.trim().parse().expect("Please Type a number");
+
+        print!("You guessed: {} ", guess);
+
+        count = count + 1;
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win! it took you {} guesses", count);
+                break;
+            }
+        }
+
+        println!("Guess again: ");
+    }
+
+}
+
+
+z
